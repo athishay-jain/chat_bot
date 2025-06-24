80ff24b3befcc57cdccdab6383faa20d93bf1c28
AJ's DESKTOP <athishayjainvenoor@gmail.com>
2025-06-24 21:57:14 +0530

    Updated UI

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:chat_bot/Screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 4), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(),)),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xff000613),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage("assets/images/black.png"),
+          image: AssetImage("assets/images/appicon_vertical.png"),
+          fadeInDuration: Duration(seconds: 2),
+        ),
+      ),
+    );
+  }
+}
